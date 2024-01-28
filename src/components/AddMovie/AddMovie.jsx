2366e99d565480591e8a4a36510b4f712269c651
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-28 16:45:52 -0600

    finished styling the add movie page

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -2,8 +2,17 @@ import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useDispatch, useSelector } from "react-redux";
 
+// MUI Imports
+import {
+    TextField,
+    Typography,
+    MenuItem,
+    Button,
+    Container
+} from "@mui/material";
+
 export default function AddMovie() {
-    const [input, setInput] = useState({})
+    const [input, setInput] = useState({ title: '', poster: '', decription: '', genre_id: '' })
 
     const genres = useSelector(store => store.genres)
 
@@ -16,9 +25,9 @@ export default function AddMovie() {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log('in handle submit');
+        // console.log('in handle submit');
         dispatch({ type: "POST_MOVIE", payload: input })
-        dispatch({type: "FETCH_MOVIES"})
+        dispatch({ type: "FETCH_MOVIES" })
         history.push('/')
     }
 
@@ -34,11 +43,11 @@ export default function AddMovie() {
     const options = () => {
         if (genres) {
             return (
-                <>
-                    {genres.map(genre => {
-                        return <option value={genre.id}>{genre.name}</option>
-                    })}
-                </>
+
+                genres.map(genre => {
+                    return <MenuItem value={genre.id} key={genre.id}>{genre.name}</MenuItem>
+                })
+
             )
         }
         return <option value="">Nothing</option>
@@ -47,24 +56,24 @@ export default function AddMovie() {
     return (
         <>
             <h2>Add a Movie</h2>
-            <p>{JSON.stringify(input)}</p>
+            {/* <p>{JSON.stringify(input)}</p> */}
             <form onSubmit={handleSubmit}>
-                <label htmlFor="titleInput">Enter Movie Title</label>
-                <input required type="text" id="titleInput" onChange={(event) => updateInput(event.target.value, 'title')} />
-                <label htmlFor="urlInput">Enter Image URL</label>
-                <input required type="text" id="urlInput" onChange={(event) => updateInput(event.target.value, 'poster')} />
-                <label htmlFor="genreInput">Select a genre</label>
-                <select required name="" id="genreInput" onChange={(event) => updateInput(event.target.value, 'genre_id')}>
-                    <option value={""}>--Genre--</option>
+                <Typography component={'label'} variant="h6" htmlFor="titleInput" display={'block'}>Enter Movie Title</Typography>
+                <TextField required label={'Enter Movie Title'} id="titleInput" display={'block'} onChange={(event) => updateInput(event.target.value, 'title')} value={input.title} />
+                <Typography component={'label'} variant="h6" htmlFor="urlInput" display={'block'} >Enter Movie Poster Image URL</Typography>
+                <TextField required label={'Enter Image URL'} id="urlInput" display={'block'} onChange={(event) => updateInput(event.target.value, 'poster')} value={input.poster} />
+
+                <Typography component={'label'} variant="h6" htmlFor="genreInput" id='genreLabel' display={'block'}>Select Genre</Typography>
+                <TextField sx={{ width: '195px' }} select required display='block' id="genreInput" label={'--Genre--'} value={input.genre_id} onChange={(event) => updateInput(event.target.value, 'genre_id')}>
                     {options()}
-                </select>
-                {/* <p>{JSON.stringify(genres)}</p> */}
-                <div>
-                    <label htmlFor="descInput">Enter Description</label>
-                    <textarea required id="descInput" cols="30" rows="10" onChange={(event) => updateInput(event.target.value, 'description')}></textarea>
-                </div>
-                <button type="submit">Add Movie</button>
-                <button type="reset" onClick={() => history.push('/')}>Cancel</button>
+                </TextField>
+                <Typography component={'label'} variant="h6" htmlFor="descInput" display={'block'}>Enter Description</Typography>
+                <TextField required id="descInput" multiline label="Description" rows={5} onChange={(event) => updateInput(event.target.value, 'description')}></TextField>
+                <Container sx={{marginTop: 5}}>
+                    <Button sx={{margin: "5px"}} type="submit" variant="contained">Add Movie</Button>
+                    <Button sx={{margin: "5px"}} type="reset" variant="outlined" onClick={() => history.push('/')}>Cancel</Button>
+                </Container>
+
             </form>
         </>
     )
